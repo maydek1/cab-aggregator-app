558117443edbd6f731404906b47138139045c9df
maydek1 <maydekcraft@gmail.com>
2024-09-17 13:47:36 +0300

    add utility class

---
+++ passenger-service/src/main/java/com/example/passengerservice/util/ExceptionMessages.java
@@ -0,0 +1,8 @@
+package com.example.passengerservice.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ExceptionMessages {
+    public static final String PASSENGER_NOT_FOUND = "Not found passenger with id: '%s'";
+}
