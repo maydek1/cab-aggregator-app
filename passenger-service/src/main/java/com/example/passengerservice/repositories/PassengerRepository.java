da95e0f76370d59cfdc323c939e2fd4c6f009096
maydek1 <maydekcraft@gmail.com>
2024-09-17 12:49:15 +0300

    created PassengerRepository

---
+++ passenger-service/src/main/java/com/example/passengerservice/repositories/PassengerRepository.java
@@ -0,0 +1,9 @@
+package com.example.passengerservice.repositories;
+
+import com.example.passengerservice.model.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PassengerRepository extends JpaRepository<Long, Passenger> {
+}
