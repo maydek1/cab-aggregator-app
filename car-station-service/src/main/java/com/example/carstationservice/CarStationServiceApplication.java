fe01185459d52416a70c912a238b5975a1ed482b
maydek1 <maydekcraft@gmail.com>
2024-09-18 15:42:38 +0300

    init car-station-service

---
+++ car-station-service/src/main/java/com/example/carstationservice/CarStationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.carstationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarStationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarStationServiceApplication.class, args);
+    }
+
+}
---
+++ car-station-service/src/test/java/com/example/carstationservice/CarStationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carstationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarStationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
