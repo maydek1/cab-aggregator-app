15a47fa14e65a123a45074746a2fb0faaba646c7
maydek1 <maydekcraft@gmail.com>
2024-09-26 17:48:36 +0300

    refactor(architecture): modify microservices communication architecture

--- car-station-service/src/test/java/com/software/modsen/carstationservice/CarStationServiceApplicationTests.java
+++ car-station-service/src/test/java/com/software/modsen/carstationservice/CarStationServiceApplicationTests.java
@@ -1,9 +1,8 @@
 package com.software.modsen.carstationservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CarStationServiceApplicationTests {
 
     @Test
