67d1b3342c5e2b0f7bb27b6104b1cefbc28e6334
maydek1 <maydekcraft@gmail.com>
2024-11-25 20:06:47 +0300

    feat(db): migrate microservice to MongoDB and implement sequence for Long ID generation

--- passenger-service/src/main/java/com/software/modsen/passengerservice/repositories/PassengerRepository.java
+++ passenger-service/src/main/java/com/software/modsen/passengerservice/repositories/PassengerRepository.java
@@ -1,19 +1,12 @@
 package com.software.modsen.passengerservice.repositories;
 
 import com.software.modsen.passengerservice.model.Passenger;
-import jakarta.persistence.LockModeType;
-import jakarta.persistence.QueryHint;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
+public interface PassengerRepository extends MongoRepository<Passenger, Long> {
     boolean existsByEmail(String email);
     boolean existsByPhone(String phone);
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @QueryHints(@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000"))
-    Passenger findByIdAndEmail(Long id, String email);
 }
